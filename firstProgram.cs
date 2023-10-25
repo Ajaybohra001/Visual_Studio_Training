eabc2bb4f5b3a054290369ea4ef3f43b81f56238
SUCCESSIVE\ajay.bohra <ajay.bohra@successive.tech>
2023-10-25 17:15:16 +0530

    First Program in Visual Studio.

---
+++ firstProgram.cs
@@ -0,0 +1,27 @@
+﻿using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Hello World!");
+
+        Console.WriteLine("Enter your Name ");
+       string ch= Console.ReadLine();
+
+        Console.WriteLine(ch);
+
+        Console.WriteLine("Enter first no");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter second no");
+
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Sum of two nos is : " + +(x + y));
+
+
+
+
+
+    }
+}
+
